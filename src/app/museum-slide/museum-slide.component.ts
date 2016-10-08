9aa721a2e79549e89e8bb452cebd5fc47f40057b
Georg Grab <talkdirty@users.noreply.github.com>
2016-10-08 21:15:32 +0200

    Initial Design Sketch

---
+++ src/app/museum-slide/museum-slide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-museum-slide',
+  templateUrl: './museum-slide.component.html',
+  styleUrls: ['./museum-slide.component.css']
+})
+export class MuseumSlideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
