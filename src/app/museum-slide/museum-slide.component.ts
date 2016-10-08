130cb8fb8fd7b8cb72b574f0572ef6f56858ec9c
Georg Grab <talkdirty@users.noreply.github.com>
2016-10-08 23:48:15 +0200

    Added chips

--- src/app/museum-slide/museum-slide.component.ts
+++ src/app/museum-slide/museum-slide.component.ts
@@ -10,11 +10,15 @@ export class MuseumSlideComponent implements OnInit {
 
   constructor() {
     this.PersonDirectory = [
-      {vorname: "Alan", nachname: "Turing",
+      {vorname: "Alan", nachname: "Turing", caption: "First human on the moon",
       portrait: {
         url : "url(http://placehold.it/600x600)",
         width: "600px", height: "600px"
       },
+      chips: [
+        {letter: "A", text: "Awesome Guy"},
+        {letter: "C", text: "Computer Scientist"}
+      ],
       slides: [
           {type: "DataTile", payload: "Born on the moon"},
           {type: "ImageTile", caption: "Bomba", src: "http://placehold.it/300x300"}
